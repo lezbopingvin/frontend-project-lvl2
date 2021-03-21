d37a9593252a0e58e9f2957a706878a0257b8294
lezbopingvin <lezbopingvin@gmail.com>
2021-03-21 11:34:09 +0300

    #3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../index.js';
 
 const program = new Command();
 program
@@ -7,7 +8,7 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
 
 program.parse();
-console.log(program.arguments);
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-import * as fs from 'fs/promises';
+import * as fs from 'fs';
+
+export default (file1, file2) => {
+  const json1 = JSON.parse(fs.readFileSync(file1, 'utf-8'));
+  const json2 = JSON.parse(fs.readFileSync(file2, 'utf-8'));
+  const key1 = Object.keys(json1);
+  const key2 = Object.keys(json2);
+  const keys = [...key1, ...key2].sort();
+  const heh = keys
+    .reduce((acc, item, index) => {
+      const arr = keys.slice(index + 1);
+      const removed = ' - ';
+      const added = ' + ';
+      const same = '   ';
+      if (arr.includes(item)) {
+        if (json1[item] === json2[item]) {
+          return [...acc, `${same}${item}: ${json1[item]}`];
+        }
+        return [...acc, `${removed}${item}: ${json1[item]}`, `${added}${item}: ${json2[item]}`];
+      }
+      if (key1.includes(item) && !key2.includes(item)) {
+        return [...acc, `${removed}${item}: ${json1[item]}`];
+      }
+      if (!key1.includes(item) && key2.includes(item)) {
+        return [...acc, `${added}${item}: ${json2[item]}`];
+      }
+      return acc;
+    }, []);
+  return ['{', ...heh, '}'].join('\n');
+};
